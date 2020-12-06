daa92ce1e932d00d790c36c05d1adc83d8b1b58b
VirtualCat <howard990523howard@gmail.com>
2020-12-06 16:49:09 +0800

    update

--- autofill/main.py
+++ autofill/main.py
@@ -1,46 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-url = 'https://ci.isu.edu.tw/prev/prev_login.asp?lang=CH&st=ISU'
-url2 = 'https://ci.isu.edu.tw/prev/prev_fill.asp'
+def loginin(user_id,user_pass):
+    url = 'https://ci.isu.edu.tw/prev/prev_login.asp?lang=CH&st=ISU'
+    url2 = 'https://ci.isu.edu.tw/prev/prev_fill.asp'
 
-user = 'isu10603017a'
-password = 'howard990523'
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
 
+    #browser = webdriver.Chrome()
+    browser = webdriver.Chrome(chrome_options=options, executable_path='./chromedriver')
 
-#user = ''
-#password = ''
+    browser.get(url)
+    browser.implicitly_wait(2)
 
-### chrome undiplay setting
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
+    elem=browser.find_element_by_name('loginID')
+    elem.send_keys(user_id)
+    elem=browser.find_element_by_name('passID')
+    elem.send_keys(user_pass)
+    elem.send_keys(Keys.ENTER)
+    browser.implicitly_wait(5)
 
-#### display
-#browser = webdriver.Chrome()
-#### undisplay
-browser = webdriver.Chrome(chrome_options=options, executable_path='./chromedriver')
+    browser.get(url2)
+    browser.implicitly_wait(2)
 
-#### url
-browser.get(url)
-browser.implicitly_wait(2)
+    fill = browser.find_element_by_id('nav_bar_txt1').click()
+    browser.implicitly_wait(2)
+    check = browser.find_element_by_id('emp_past14_10').click()
+    sumbit = browser.find_element_by_id('submit_btn').click()
 
-### loging
-elem=browser.find_element_by_name('loginID')
-elem.send_keys(user)
-elem=browser.find_element_by_name('passID')
-elem.send_keys(password)
-elem.send_keys(Keys.ENTER)
-browser.implicitly_wait(5)
+    browser.quit()
 
-browser.get(url2)
-browser.implicitly_wait(2)
+f1 = open('.\\log\\user.txt', 'r', encoding='utf-8')
+f2 = open('.\\log\\pass.txt', 'r', encoding='utf-8')
 
-### autofill
-## 畫面過大時id為 nav_bar_txt2 
-fill = browser.find_element_by_id('nav_bar_txt1').click()
-browser.implicitly_wait(2)
-check = browser.find_element_by_id('emp_past14_10').click()
-sumbit = browser.find_element_by_id('submit_btn').click()
+l1 = [(line.strip()).split() for line in f1]
+l2 = [(line.strip()).split() for line in f2]
 
-### close
-browser.quit()
+for i in range(len(l1)):
+    loginin(l1[i],l2[i])   
+    print("OK")
